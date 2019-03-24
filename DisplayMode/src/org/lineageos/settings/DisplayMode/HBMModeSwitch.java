84505b6794541ec0b807fcca62646c6051e2276c
Ralf Luther <luther.ralf@gmail.com>
2019-03-24 17:10:39 +0100

    op-common: minor platform adaptions III
    Damn all derps.

--- DisplayMode/src/org/lineageos/settings/DisplayMode/HBMModeSwitch.java
+++ DisplayMode/src/org/lineageos/settings/DisplayMode/HBMModeSwitch.java
@@ -27,7 +27,7 @@ import org.lineageos.settings.DisplayMode.DeviceSettings;
 
 public class HBMModeSwitch implements OnPreferenceChangeListener {
 
-    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
+    private static final String FILE_SDM = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
     private static final String FILE_MSM = "/sys/devices/virtual/graphics/fb0/hbm";
 
     public static String getFile() {
