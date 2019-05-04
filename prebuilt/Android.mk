a3ee35ec1103af09711d31c2aa75f1096aeeb1d3
Ralf Luther <luther.ralf@gmail.com>
2019-05-04 12:33:38 +0200

    op-common: create a separte init file
    create a separate init file for sdm devices that include only the parts necessary for them and filter on build to copy it to the apropiate location.

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),enchilada fajita))
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.oneplus.rc
+LOCAL_MODULE       := init.oneplus-sdm.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := prebuilt/vendor/etc/init/hw/init.oneplus.rc
+LOCAL_SRC_FILES    := prebuilt/init/init.oneplus-sdm.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 endif
