3ceb6133441cb132eaf55ddac4f91e684a532d8d
Ralf Luther <luther.ralf@gmail.com>
2019-05-30 18:20:59 +0200

    op-common: update for DeviceHandler * make it optional * make it a priviliged module * make it compile with AAPT2 * add static libs * add the path to the res

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -19,9 +19,24 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := DeviceHandler
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-LOCAL_DEX_PREOPT := false
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
 
 include $(BUILD_PACKAGE)
